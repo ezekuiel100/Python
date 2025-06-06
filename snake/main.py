08c3da8ead750c34b8d3aded0bad43ecc6473e4e
unknown <ezekuiel100@gmail.com>
2025-06-06 19:10:55 -0300

    add snake growth after eating food

--- snake/main.py
+++ snake/main.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import ttk
 
 snake_direction = "Down"
+snake_parts = []
+
 
 def draw_canvas():
    tamanho_bloco = 20 
@@ -15,6 +17,9 @@ def draw_canvas():
         x2 = x1  + tamanho_bloco 
         y2 = y1 + tamanho_bloco
         canvas.create_rectangle(x1, y1, x2 , y2, outline="white")
+
+def create_food(): 
+ pass
 
 
 def change_snake_direction(event):
@@ -31,15 +36,34 @@ def change_snake_direction(event):
      
 
 def move_snake(): 
+   head_coords = canvas.coords(snake_parts[0])
+   x1, y1, x2, y2 = head_coords
+
+
    if(snake_direction == "Up"):      
-      canvas.move(snake , 0 , -20)
+      y1 -= 20
+      y2 -= 20
    elif(snake_direction == "Down"):
-      canvas.move(snake , 0 , +20)
+       y1 += 20
+       y2 += 20
    elif(snake_direction == "Left"):
-      canvas.move(snake , -20, 0 )
+       x1 -= 20
+       x2 -= 20
    elif( snake_direction == "Right"):
-     canvas.move(snake ,+20, 0)
+       x1 += 20
+       x2 += 20
    
+   new_part = canvas.create_rectangle(x1, y1, x2, y2 , fill='green')
+   snake_parts.insert(0,new_part)
+
+   food_coords = canvas.coords(food)
+   if head_coords[0] == food_coords[0] and head_coords[1] == food_coords[1]:
+      create_food()
+   else:
+      tail = snake_parts.pop()
+      canvas.delete(tail)
+
+
    window.after(100, move_snake)
 
      
@@ -55,7 +79,9 @@ window.geometry(f"{screen_width}x{screen_height}")
 canvas = Canvas(window, width=screen_width, height=screen_height , background='gray75')    
            
 snake = canvas.create_rectangle(20, 20, 40, 40, fill='blue')
-canvas.create_oval(40, 40, 60, 60, fill='red')
+snake_parts.append(snake)
+
+food = canvas.create_oval(40, 40, 60, 60, fill='red')
 
 window.bind("<Key>" , change_snake_direction)
 
